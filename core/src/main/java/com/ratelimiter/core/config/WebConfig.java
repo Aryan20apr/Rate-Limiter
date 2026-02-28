2c06dc0652fd9b9cd88f1bf1a5ac6967f2bbb6b4
Aryan Singh <aryansingh20nov@gmail.com>
2026-03-01 00:34:17 +0530

    Bug fix

--- core/src/main/java/com/ratelimiter/core/config/WebConfig.java
+++ core/src/main/java/com/ratelimiter/core/config/WebConfig.java
@@ -1,5 +1,11 @@
 package com.ratelimiter.core.config;
 
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ratelimiter.core.service.RateLimitManager;
+import com.ratelimiter.core.web.RateLimitInterceptor;
+
 // @Configuration -- Not enabled for now as filter will work
 public class WebConfig implements WebMvcConfigurer {
 
